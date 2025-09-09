fccf9ada96e0d1858e0f931456f434abb6b29c2a
elerazo- <chaconnayeli818@gmail.com>
2025-09-09 19:25:09 +0200

    .h

--- Inc/philosophers.h
+++ Inc/philosophers.h
@@ -6,11 +6,15 @@
 /*   By: elerazo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 17:23:05 by elerazo-          #+#    #+#             */
-/*   Updated: 2025/09/08 19:56:30 by elerazo          ###   ########.fr       */
+/*   Updated: 2025/09/09 19:24:39 by elerazo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
+
+/* ************************************************************************** */
+/*                                 MACROS                                    */
+/* ************************************************************************** */
 
 # define SATISFIED 1
 # define UNSATISFIED 0
@@ -25,7 +29,19 @@
 # define STR_FORK2 "Relase both forks 🍴\033[0m"
 # define STR_DIE "Dying 💀\033[0m" 
 
-//# <biblioteca.h>
+/* ************************************************************************** */
+/*                                 STRUCTS                                   */
+/* ************************************************************************** */
+
+typedef struct s_info
+{
+
+}				t_info;
+
+/* ************************************************************************** */
+/*                            BIBLIOTECAS.H                                   */
+/* ************************************************************************** */
+
 # include <unistd.h>
 # include <fcntl.h>
 # include <sys/types.h>
@@ -33,29 +49,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <pthread.h>
-//# include "../library/printf/ft_printf.h"
-//# include "../library/libft/libft.h"
 
-//structura
-typedef struct s_pipex
-{
-	char	*infile;
-	char	*outfile;
-	char	*cmd1_path;
-	char	*cmd2_path;
-	char	*path;
-	char	**args;
-	char	**envp;
-	char	**cmd1_args;
-	char	**cmd2_args;
-	int		infile_fd;
-	int		outfile_fd;
-	int		pipe_fd[2];
-	int		fd[2];
-	int		doc[2];
-	int		pid[2];
-}				t_pipex;
-
-//prototipo de funciones
+/* ************************************************************************** */
+/*                     PROTOTYPE OF FUNTION                                   */
+/* ************************************************************************** */
 
 #endif
